fixing targets and weighting shapes for new loss api

rhn.py
@@ -58,11 +58,11 @@ class Model(object):
     softmax_b = tf.get_variable("softmax_b", [vocab_size])
     logits = tf.matmul(output, softmax_w) + softmax_b
     logits = tf.reshape(logits, [-1, 1, vocab_size])
-    
+
     loss = tf.contrib.seq2seq.sequence_loss(
       logits,
-      tf.reshape(self._targets, [-1]),
-      tf.ones([batch_size * num_steps]))
+      self._targets,
+      tf.ones([batch_size, num_steps]))
     self._final_state = [s[0] for s in state]
     pred_loss = tf.reduce_sum(loss) / batch_size
     self._cost = cost = pred_loss